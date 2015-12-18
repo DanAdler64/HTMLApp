8ea2ba17ca5537c955c5ba0b6429b03d0577504b
DanAdler <daadler64@gmail.com>
2015-12-18 10:38:07 -0500

    🔥 Update!!

--- app/src/main/java/com/onyxlabs/app/htmlapp/MainActivity.java
+++ app/src/main/java/com/onyxlabs/app/htmlapp/MainActivity.java
@@ -10,8 +10,8 @@ import com.onyxlabs.app.htmlapp.R;
 import com.crashlytics.android.Crashlytics;
 import io.fabric.sdk.android.Fabric;
 
-public class MainActivity extends ActionBarActivity {
-
+public class MainActivity extends ActionBarActivity
+{
     /**CONSTANTS**/
 
     private static final String TAG = MainActivity.class.getCanonicalName();
@@ -23,21 +23,8 @@ public class MainActivity extends ActionBarActivity {
     private ProgressBar mLoading;
 
 
-/**
-    protected void onCreate(Bundle savedInstanceState) {
-
-        super.onCreate(savedInstanceState);
-
-        setContentView(R.layout.activity_main);
-
-        mWebView = (WebView)findViewById(R.id.wvPortal);
-
-        mLoading = (ProgressBar) findViewById(R.id.pbLoading);
-
-    }
- */
-protected void onCreate(Bundle savedInstanceState) {
-
+protected void onCreate(Bundle savedInstanceState)
+{
     super.onCreate(savedInstanceState);
     Fabric.with(this, new Crashlytics());
 
@@ -51,7 +38,11 @@ protected void onCreate(Bundle savedInstanceState) {
 
     WebSettings mWebSettings = mWebView.getSettings();
 
+    // Misc. settings
     mWebSettings.setJavaScriptEnabled(true);
+    mWebSettings.setAppCacheEnabled(true);
+    mWebSettings.setLoadsImagesAutomatically(true);
+    mWebSettings.setBuiltInZoomControls(false);
 
 
 
